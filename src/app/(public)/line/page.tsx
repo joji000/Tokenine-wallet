1c5abba7cfc2e1f2e600e49b460d4e3499d3692e
joji <172300802+joji000@users.noreply.github.com>
2025-02-25 01:31:27 +0700

    feat:line auth callback

--- src/app/(public)/line/page.tsx
+++ src/app/(public)/line/page.tsx
@@ -1,35 +1,49 @@
 'use client';
 import React, { useEffect } from "react";
+import { useRouter } from 'next/navigation';
 import { initLiff, liff } from '@/libs/line.lib';
 
 const LinePage = () => {
+  const router = useRouter();
 
-    useEffect(() => {
-        initLiff().then(() => {
-            handlelogin();
-        });
-    
-    }, []);
+  useEffect(() => {
+    initLiff().then(() => {
+      handleLogin();
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const handlelogin = async () => {
-        try{
+  const handleLogin = async () => {
+    try {
+      const isLoggedIn = await liff.isLoggedIn();
+      if (!isLoggedIn) {
+        liff.login();
+      }
 
-            const isLoggedIn = await liff.isLoggedIn();
-            console.log(isLoggedIn);
-            if(!isLoggedIn){
-                liff.login();
-            }
+      const idToken = liff.getIDToken();
+      console.log("ID Token:", idToken);
 
-            const profile = await liff.getProfile();
-            console.log(profile);
-            const idToken = liff.getIDToken();
-            console.log(idToken);
-            const decodedIdToken = liff.getDecodedIDToken();
-            console.log(decodedIdToken?.email);
-        }catch(error){
-            console.log(error);
-        }
+      // Send the ID token to the server
+      const response = await fetch('/api/auth/callback/line', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ idToken }),
+      });
+
+      if (response.ok) {
+        const { redirectTo } = await response.json();
+        console.log("Server Response:", redirectTo);
+        router.push(redirectTo);
+      } else {
+        const errorText = await response.text();
+        console.error('Failed to authenticate with the server:', errorText);
+      }
+    } catch (error) {
+      console.error('Error during LIFF login:', error);
     }
+  };
 
   return (
     <div>
